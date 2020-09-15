e4b6e1eeee1edee7753430e074be354f47185ede
Franz Kirsten <franz.kirsten@chalmers.se>
2020-09-15 16:49:12 +0200

    first working version of vex-parser. Reworked to actually create a config file that is then passed to the pipeline.

--- parse_vex.py
+++ parse_vex.py
@@ -10,13 +10,30 @@ def options():
     general.add_argument('vexfile', type=str,
                          help='vexfile used for the experiment')
     general.add_argument('-s', '--source', type=str, required=True,
-                         help='Source for which data are to be analysed.')
+                         help='REQUIRED. Source for which data are to be analysed.')
     general.add_argument('-t', '--telescope', type=str, required=True,
-                         choices=['o8', 'o6', 'sr', 'wb', 'ef', 'tr'],
-                         help='2-letter Station code of dish to be searched.')
-    general.add_argument('-S', '--scans', nargs='+', default=None,
+                         choices=['o8', 'o6', 'sr', 'wb', 'ef', 'tr', \
+                                  'onsala85', 'onsala60', 'srt',\
+                                  'wsrt', 'effelsberg', 'torun'],
+                         help='REQUIRED. Station name or 2-letter code of dish to be searched.')
+    general.add_argument('-S', '--scans', nargs='+', default=None, type=int,
                          help='Optional list of scans to be searched. By default will ' \
-                         'return all scans. Scan numbers without leading zeros please.')
+                         'return all scans. Scan numbers with or without leading zeros.')
+    general.add_argument('-n', '--nchan', type=int, default=128,
+                         help='Number of channels per subband (i.e. per IF). Default=%(default)s')
+    general.add_argument('-d', '--downsamp', type=int, default=1,
+                         help='Downsampling factor for final filterbanks. Default '\
+                         'is no downsampling.')
+    general.add_argument('-o', '--outfile', type=str, default=None,
+                         help='Name of the config file. Per default will be named ' \
+                         '<experiment>_<station>_<source>.conf[_<mode>], where ' \
+                         '_<mode> will only be appended in case there a multiple '\
+                         'frequency setups.')
+    general.add_argument('-T', '--template', type=str, default=None,
+                         help='Template config file that contains parameters beyond '\
+                         'those taken care of here. Existing parameters that this '\
+                         'script takes care of will be overwritten.')
+    
     return parser.parse_args()
 
 
@@ -43,12 +60,12 @@ def vex2dic(vexfile):
             VEX[key] = vex[line_num+1:]
     return VEX
 
-def get_freq(vexdic, station, mode):
+def getFreq(vexdic, station, mode):
     '''
     Returns the reference frequency, bandwidth, and number of IFs for station in mode.
     '''
     lines = vexdic['MODE']
-    station = station.capitalize()
+    station = fixStationName(station).capitalize()
     mode = mode.upper()
     start_lineNums = [i for i,line in enumerate(lines) if line.startswith('def')]
     stop_lineNums = [i for i,line in enumerate(lines) if line.startswith('enddef')]
@@ -58,15 +75,32 @@ def get_freq(vexdic, station, mode):
                 line = lines[lineNum]
                 if ('FREQ' in line) and (station in line):
                     f_info = line.split('=')[1].strip().split('MHz')
-                    f_ref = f_info[0]
+                    f_ref = float(f_info[0])
                     n_if, bw = f_info[1].split('x')
                     n_if = str(int(n_if) // 2)
+                    bw = float(bw)
                     break
             break
     try:
         return f_ref, bw, n_if
     except:
         raise RunError(f'Could not determine Frequency setup for {station} in {mode}.')
+
+def getExperimentName(vexdic):
+    '''
+    Returns the name of the experiment as defined in the vexfile.
+    '''
+    lines = vexdic['EXPER']
+    start_lineNums = [i for i,line in enumerate(lines) if line.startswith('def')]
+    stop_lineNums = [i for i,line in enumerate(lines) if line.startswith('enddef')]
+    if len(start_lineNums) > 1:
+        raise RunError(f'Found more than one experiment name in vexfile. Not sure how to deal with this...')
+    for lineNum in range(start_lineNums[0]+1,stop_lineNums[0]):
+        line = lines[lineNum]
+        if 'exper_name' in line:
+            experiment = line.split('=')[1].strip().replace(';','')
+            break
+    return experiment
 
 def getSourceCoords(vexdic, source):
     '''
@@ -152,7 +186,7 @@ def getScanList(df, source, station, mode, scans=None):
     at the beginning of the scan, and number of secodns to process.
     '''
     mode = mode.upper()
-    station = station.capitalize()
+    station = fixStationName(station).capitalize()
     ddf = df[(df.source == source) &
              (df.fmode == mode) &
              (df.station == station)]
@@ -171,8 +205,8 @@ def getScanList(df, source, station, mode, scans=None):
             scan -= 1
             skip_sec += df[(df.scanNo == scan) &
                            (df.station == station)].length_sec.item()
-        skip_secs.append(skip_sec-1)
-        start_scans.append(scan)
+        skip_secs.append(skip_sec-1 if skip_sec > 0 else skip_sec)
+        start_scans.append(f'{scan:03d}')
     if not len(start_scans) == len(skip_secs) == len(scan_lengths):
         raise RunError('Not the same number of scans, seconds to skip and scan lengths.')
     return start_scans, skip_secs, scan_lengths
@@ -203,8 +237,77 @@ class RunError(Error):
     def __init__(self, message):
         self.message = message
 
+def writeConfig(outfile, experiment, source, station,
+                ra, dec, fref, bw, nIF, nchan, downsamp,
+                scans, skips, lengths,
+                template=None):
+    templ = []
+    if not template == None:
+        if not os.path.exists(template):
+            raise InputError(f'The supplied template file {template} does not exist.')
+        f = open(template, 'r')
+        templ = f.readlines()
+        f.close()
+        params = ['experiment', 'target', 'scans', 'skips',
+                  'lenghts', 'freqLSB_0', 'bw', 'nif',
+                  'nchan', 'tscrunch', 'station']
+        # we overwrite existing parameters
+        delLines = [i for param in params for i,line in enumerate(templ) if param in line]
+        templ = [line for i,line in enumerate(templ) if i not in delLines]
+    scans = list2BashArray(scans)
+    skips = list2BashArray(skips)
+    lengths = list2BashArray(lengths)
+    station = fixStationName(station, short=False)
+    templ.append(f'experiment={experiment}\n')
+    templ.append(f'target=\"{source} --ra {ra} --dec {dec}\"\n')
+    templ.append(f'station={station}\n')
+    templ.append(f'scans={scans}\n')
+    templ.append(f'skips={skips}\n')
+    templ.append(f'lengths={lengths}\n')
+    templ.append(f'freqLSB_0={fref-bw/2}\n')
+    templ.append(f'bw={bw}\n')
+    templ.append(f'nif={nIF}\n')
+    templ.append(f'nchan={nchan}\n')
+    templ.append(f'tscrunch={downsamp}\n')
+    with open(outfile, 'w') as f:
+        for line in templ:
+            f.write(line)
+    return
+    
+    
+def list2BashArray(l):
+    '''
+    Takes a regular list and turns it into a bash array for scripting.
+    '''
+    if not isinstance(l, list):
+        try:
+            l = list(l)
+        except:
+            raise InputError(f'Supplied object cannot be turned into a list. l={l}.')
+    array = '( '
+    for i in l:
+        array = f'{array}{i} '
+    array = f'{array})'
+    return array
+
+def fixStationName(station, short=True):
+    '''
+    Returns the 2-letter station code for station if short=True, else the 
+    long version that is TEMPO2-compliant.
+    '''
+    station = station.lower()
+    longnames = ['onsala85', 'onsala60', 'srt', 'wsrt', 'effelsberg', 'torun']
+    shortnames = ['o8', 'o6', 'sr', 'wb', 'ef', 'tr']
+    if not (station in longnames) and not (station in shortnames):
+        raise InputError(f'Station {station} not recognized. ' \
+                         f'Must be any of {longnames} or {shortnames}')
+    if len(station) > 2:
+        return shortnames[longnames.index(station)] if short else station
+    else:
+        return station if short else longnames[shortnames.index(station)]
+    
 def main(args):
-    vexfile = args.vexfile
+    vexfile = os.path.abspath(args.vexfile)
     vex = vex2dic(vexfile)
     # per default we expect the dataframe to be in the same dir
     # as the vexfile. If it's not there we create it such that
@@ -217,24 +320,43 @@ def main(args):
         df.to_pickle(df_file)
     else:
         df = pd.read_pickle(df_file)
+    experiment = getExperimentName(vex)
+    print(f'Found experiment {experiment}.')
     fmodes = list(df.fmode.unique())
     print(f'There are {len(fmodes)} frequency modes: {fmodes}')
-    source = args.source.replace('_D','')
-    station = args.telescope
     ra, dec = getSourceCoords(vex, args.source)
+    source = args.source.replace('_D','').upper()
+    station = args.telescope
+    nchan = args.nchan
+    downsamp = args.downsamp
+    outfile = args.outfile
+    template = args.template
+    if outfile == None:
+        outfile = f'{sched_dir}/{experiment}_{station}_{source}.conf'
     for fmode in fmodes:
+        if len(fmodes) > 1:
+            outfile = f'{outfile}_{fmode}'
         try:
-            fref, bw, nIF = get_freq(vex, station, fmode)
+            fref, bw, nIF = getFreq(vex, station, fmode)
         except:
             print(f'No setup for station {station} in mode {fmode}.')
             continue
-        scans, skips, lengths = getScanList(df, source,
-                                            station, fmode,
-                                            scans=args.scans)
-        
+        try:
+            scans, skips, lengths = getScanList(df, source,
+                                                station, fmode,
+                                                scans=args.scans)
+        except:
+            print(f'Found no data for {source} for {station} in {fmode}.')
+        try:
+            writeConfig(outfile, experiment, source, station, ra, dec,
+                        fref, bw, nIF, nchan, downsamp, scans, skips, lengths,
+                        template)
+        except:
+            raise RunError(f'Could not create config file.')
     return
         
         
 if __name__ == "__main__":
     args = options()
     main(args)
+    
