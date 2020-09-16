9f77aabb5dd096f306f806b32bc7521753122178
Franz Kirsten <franz.kirsten@chalmers.se>
2020-09-16 23:08:52 +0200

    fixed bug in the counter (i.e. scancounter); reworked things to have output be names after scan in vex and sum files instead of numbers skipped from beginning of recorded scan in case the gap was too short to start a new recording.

--- pipeline_multiScan_multiChunk.sh
+++ pipeline_multiScan_multiChunk.sh
@@ -10,7 +10,7 @@ pwait() {
 run_process_vdif() {
     # assumes that the IFs passed to the function are all of the same sideband
     # and increase in frequency by 2 x BW
-    scan=$1
+    scanname=$1
     ifs=$2
     source=$3
     experiment=$4
@@ -32,7 +32,7 @@ run_process_vdif() {
     bandstep=`echo $bw+$bw | bc`
 
     for i in ${ifs};do
-        /home/franz/git/frb-baseband/process_vdif.py ${source} ${workdir}/${experiment}_${st}_no0${scan}_IF${i}_skip${skip}s.vdif  \
+        /home/franz/git/frb-baseband/process_vdif.py ${source} ${workdir}/${experiment}_${st}_no0${scanname}_IF${i}.vdif  \
                                             -f $freqEdge -b ${bw} -${sideband} --nchan $nchan --nsec $nsec --start $start \
                                             --force -t ${station} --pol ${pol} --nthreads ${nthreads} --tscrunch ${tscrunch} --fil_out_dir ${fifodir} & sleep 0.2
         pwait $njobs
@@ -114,6 +114,7 @@ start=0 #
 scans=( 001 002 )
 skips=( 0 0 )
 lengths=( 10 10 )
+scannames=( 001 002 )
 
 nchan=256       # number of channels in filterbank
 nif=8           # number of subbands in the data, 16 at OSO at 4G recording, 8 at Tr or Sr at 2G recording
@@ -192,20 +193,21 @@ if [ ${n_baseband_files} -eq 1 ];then
 fi
 msg "There are ${n_baseband_files} baseband files in ${vbsdir}"
 
-counter=-1
+scancounter=-1
 for scan in "${scans[@]}";do
-    let counter=${counter}+1
-    skip=${skips[${counter}]}
-    length=${lengths[${counter}]}
+    let scancounter=${scancounter}+1
+    skip=${skips[${scancounter}]}
+    length=${lengths[${scancounter}]}
+    scanname=${scannames[${scancounter}]}
     vdif_files=""
     splice_list=''
     for i in `seq 1 2 ${nif}`;do
         # odd IFs first
-        vdifnme=${workdir_odd}/${experiment}_${st}_no0${scan}_IF${i}_skip${skip}s.vdif
+        vdifnme=${workdir_odd}/${experiment}_${st}_no0${scanname}_IF${i}.vdif
         vdif_files=${vdif_files}${vdifnme}" "
         if [ ! -f ${vdifnme} ];then
             msg "Splitting the VDIF."
-            ${spif2file} ${experiment} ${st} ${scan} ${nif} ${mode} ${skip} ${length} ${flipIF}
+            ${spif2file} ${experiment} ${st} ${scan} ${nif} ${mode} ${skip} ${length} ${scanname} ${flipIF}
     	if [[ $? -eq 1 ]];then
     	    exit 1
     	fi
@@ -217,11 +219,11 @@ for scan in "${scans[@]}";do
         splice_list=${filfifo}' '${splice_list}
         # then even IFs
         let n=$i+1
-        vdifnme=${workdir_even}/${experiment}_${st}_no0${scan}_IF${n}_skip${skip}s.vdif
+        vdifnme=${workdir_even}/${experiment}_${st}_no0${scanname}_IF${n}.vdif
         vdif_files=${vdif_files}${vdifnme}" "
         if [ ! -f ${vdifnme} ];then
             msg "Splitting the VDIF on Bogar for even IFs."
-            ${spif2file} ${experiment} ${st} ${scan} ${nif} ${mode} ${skip} ${length} ${flipIF}
+            ${spif2file} ${experiment} ${st} ${scan} ${nif} ${mode} ${skip} ${length} ${scanname} ${flipIF}
     	if [[ $? -eq 1 ]];then
     	    exit 1
     	fi
@@ -241,29 +243,29 @@ for scan in "${scans[@]}";do
     fi
     file_size=`ls -l ${vdifnme} | cut -d ' ' -f 5`
     nsec=`echo "${file_size}/${frame_size}/${frames_per_second_per_band}" | bc`
-    run_process_vdif $scan "$ifs_odd" "$target" $experiment $st $freqLSB_0 $bw l $nchan $nsec $start \
+    run_process_vdif $scanname "$ifs_odd" "$target" $experiment $st $freqLSB_0 $bw l $nchan $nsec $start \
                       $station $njobs_splice $skip $workdir_odd $pol $digifil_nthreads $tscrunch ${fifodir}
     # even IFs (i.e. USB)
-    run_process_vdif $scan "$ifs_even" "$target" $experiment $st $freqUSB_0 $bw u $nchan $nsec $start \
+    run_process_vdif $scanname "$ifs_even" "$target" $experiment $st $freqUSB_0 $bw u $nchan $nsec $start \
                       $station $njobs_splice $skip $workdir_even $pol $digifil_nthreads $tscrunch ${fifodir}
 
-    filfile=${experiment}_${st}_no0${scan}_IFall_skip${skip}s_vdif_pol${pol}.fil
+    filfile=${experiment}_${st}_no0${scanname}_IFall_vdif_pol${pol}.fil
     # increase the fifo buffer size to speed things up, but wait till splice is running first
     sleep 2 && for filfifo in ${splice_list}; do \
         /home/franz/git/frb-baseband/setfifo.perl ${filfifo} 1048576; \
         sleep 0.2;done && msg "Changed fifo sizes successfuly." &
     if [[ $submit2fetch -ne 0 ]]; then
 	splice ${splice_list} > ${outdir}/${filfile} && \
-            rm -rf ${workdir_even}/${experiment}_${st}_no0${scan}_IF*_skip${skip}s.vdif \
-               ${workdir_odd}/${experiment}_${st}_no0${scan}_IF*_skip${skip}s.vdif && \
+            rm -rf ${workdir_even}/${experiment}_${st}_no0${scanname}_IF*.vdif \
+               ${workdir_odd}/${experiment}_${st}_no0${scanname}_IF*.vdif && \
             submit_fetch ${outdir}/${filfile} && \
             msg "Submitted ${outdir}/${filfile} to fetch" && \
             for filfifo in ${splice_list};do rm -rf $filfifo; done && \
             msg "Fifos removed" &
     else
 	splice ${splice_list} > ${outdir}/${filfile} && \
-            rm -rf ${workdir_even}/${experiment}_${st}_no0${scan}_IF*_skip${skip}s.vdif \
-               ${workdir_odd}/${experiment}_${st}_no0${scan}_IF*_skip${skip}s.vdif && \
+            rm -rf ${workdir_even}/${experiment}_${st}_no0${scanname}_IF*.vdif \
+               ${workdir_odd}/${experiment}_${st}_no0${scanname}_IF*.vdif && \
             for filfifo in ${splice_list};do rm -rf $filfifo; done && \
             msg "Fifos removed" &
     fi
@@ -276,8 +278,8 @@ if [ $? -eq 0 ];then
     counter=-1
     for scan in "${scans[@]}";do
 	let counter=${counter}+1
-	skip=${skips[${counter}]}
-        filfile=${experiment}_${st}_no0${scan}_IFall_skip${skip}s_vdif_pol${pol}.fil
+	scanname=${scannames[${counter}]}
+        filfile=${experiment}_${st}_no0${scanname}_IFall_vdif_pol${pol}.fil
         dspsr -E ${target}.psrcat.par -L 10 -A -k ${station} -d1 ${outdir}/${filfile} -O ${outdir}/${filfile} -t 8
         psrplot -pF -D /CPS -c x:unit=s ${outdir}/${filfile}.ar -j dedisperse,tscrunch,pscrunch,"fscrunch 128"
         mv pgplot.ps ${outdir}/${filfile}.ps
