b26c49f04be37a5e9c7e36991699c86e636aaab4
kirsten@drg22 <pharao@posteo.net>
2022-10-13 14:37:02 +0000

    fixed mor identation issues

--- cmd2flexbuff.py
+++ cmd2flexbuff.py
@@ -10,13 +10,13 @@ import socket
 
 # Infos
 def usage_exit():
-	print('Usage: stopfila10g.py [-u|--udp] <ip_address> <ip_port>')
-	sys.exit(-1)
+        print('Usage: stopfila10g.py [-u|--udp] <ip_address> <ip_port>')
+        sys.exit(-1)
 
 # Return a socket:  conn=(ip,port,use_tcp)
 def getSocket(conn):
-	try:
-		if conn['isTCP']:
+        try:
+                if conn['isTCP']:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((conn['ip'],conn['port']))
 		else:
@@ -59,15 +59,15 @@ def main(argv):
         if (conn==None):
                 os.system("inject_snap \'\"Error opening connection to flexbuff\'")
 	        sys.exit(-1)
-
+                
         # First stop sending data
         # Append data source command(s)
         reply=sendRcv(conn, " ".join(argv[1:]) + '\r\n')
         lines=reply.strip('\r\n').splitlines()
         for line in lines:
-	        print("Flexbuff: "+line)
+                print("Flexbuff: "+line)
         sys.exit(0)	
 
 if __name__ == "__main__":
-    main(sys.argv)
+        main(sys.argv)
 
