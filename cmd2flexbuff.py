ea9e2059ffb3d8ccc97300068d1f1655b619fc20
Franz Kirsten <franz@sleipnir>
2023-11-24 15:25:50 +0000

    made cmd2flexbuff.py python3 compatible

--- cmd2flexbuff.py
+++ cmd2flexbuff.py
@@ -10,7 +10,7 @@ import socket
 
 # Infos
 def usage_exit():
-	print 'Usage: stopfila10g.py [-u|--udp] <ip_address> <ip_port>'
+	print('Usage: stopfila10g.py [-u|--udp] <ip_address> <ip_port>')
 	sys.exit(-1)
 
 # Return a socket:  conn=(ip,port,use_tcp)
@@ -24,14 +24,14 @@ def getSocket(conn):
 		s.settimeout(1.0)
 		conn['socket'] = s
 	except:
-		print 'Connection to %s:%u failed' % (conn['ip'],conn['port'])
+		print('Connection to %s:%u failed' % (conn['ip'],conn['port']))
 		return None
 	return conn
 
 # Send a command, wait for reply
 def sendRcv(conn,cmdstr):
 	buffer_size = 2048
-	cmdstr = '\n' + cmdstr.strip() + '\n'
+	cmdstr = str.encode('\n' + cmdstr.strip() + '\n')
 	if conn['isTCP']:
 		conn['socket'].send(cmdstr)
 	else:
@@ -47,29 +47,28 @@ def sendRcv(conn,cmdstr):
 
 # Prepare and send commands to FILA10G
 def main(argv):
-	global _cmds
+        global _cmds
         IP = os.environ['FLEXIP']
         PORT = int(os.environ['FLEXPORT'])
-	conn_TCP = True
-	conn_IPaddr = IP
-	conn_IPport = PORT
-
-	conn = {'ip':conn_IPaddr, 'port':conn_IPport, 'isTCP':conn_TCP, 'socket':None}
-	conn = getSocket(conn)
-	if (conn==None):
-	        os.system("inject_snap \'\"Error opening connection to flexbuff\'")
-		sys.exit(-1)
+        conn_TCP = True
+        conn_IPaddr = IP
+        conn_IPport = PORT
+        conn = {'ip':conn_IPaddr, 'port':conn_IPport, 'isTCP':conn_TCP, 'socket':None}
+        conn = getSocket(conn)
+        if (conn==None):
+                os.system("inject_snap \'\"Error opening connection to flexbuff\'")
+                sys.exit(-1)
 
         # First stop sending data
 	# Append data source command(s)
-	reply=sendRcv(conn, " ".join(argv[1:]) + '\r\n')
-	lines=reply.strip('\r\n').splitlines()
-	for line in lines:
-		print "Flexbuff: "+line
-#	reply.strip('\r\n')
-#        os.system("inject_snap \'\"command sent\'")
-	sys.exit(0)	
+        reply=sendRcv(conn, " ".join(argv[1:]) + '\r\n')
+        lines=reply.strip('\r\n').splitlines()
+        for line in lines:
+                print("Flexbuff: "+line)
+        #	reply.strip('\r\n')
+        #        os.system("inject_snap \'\"command sent\'")
+        sys.exit(0)	
 
 if __name__ == "__main__":
-    main(sys.argv)
+        main(sys.argv)
 
