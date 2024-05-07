81aca7290b920f6c116fce8292ed0e7829af1f0d
Franz Kirsten <franz.kirsten@chalmers.se>
2024-05-07 17:08:05 +0200

    bug fixes to online-deamon. It actually works now.

--- online-deamon.sh
+++ online-deamon.sh
@@ -12,7 +12,7 @@
 #   - http://www.faqs.org/faqs/unix-faq/programmer/faq/
 # - not sure, maybe a full deamon is overkill? Could just have script that runs in a tmux pane somewhere. And have that start up via tmuxinator
 
-joblist=${1:-/tmp/jobs.txt}
+joblist=${1:-/tmp/joblist.txt}
 maxjobs=${2:-38}
 
 if ! [ -f ${joblist} ];then
@@ -23,7 +23,7 @@ fi
 nlines_start=$(wc -l $joblist | cut -d ' ' -f1)
 
 # create an array for the jobs to be processed
-declare -a jobs
+declare -a jobarray
 
 # we check for new entries to the joblist-file every 10s. If there's a new entry
 # we add it to our array;
@@ -37,18 +37,25 @@ while true; do
         for i in `seq ${n_new_jobs} -1 1`;do
             # entries below contains both the config file and the number of IFs
             linenum=$(echo "${nlines}-${i}+1" | bc -l)
-            jobs[${#jobs[@]}]=$(sed "${linenum}q;d" ${joblist})
+            jobarray[${#jobarray[@]}]=$(sed "${linenum}q;d" ${joblist})
         done
     fi
-    job=${jobs[0]}
-    config_file=$(cut -d ' ' -f1 ${job})
-    n_subbands=$(cut -d ' ' -f2 ${job})
+    job=${jobarray[0]}
+    if ! [ -n "$job" ];then
+        echo 'no jobs but '
+        echo "we see $(ps -ef | grep digifil | grep -v /bin/sh | wc -l) jobs running"
+        sleep 10
+        continue
+    fi
+    config_file=$(cut -d ' ' -f1 <<< ${job})
+    n_subbands=$(cut -d ' ' -f2 <<< ${job})
     njobs_running=$(ps -ef | grep digifil | grep -v /bin/sh | wc -l)
     if [ $(echo "${n_subbands}+1+${njobs_running}" | bc -l) -lt ${maxjobs} ];then
         #submit the job
+        echo "submitting ${config_file}"
         base2fil ${config_file}
         # drop that first entry from the array
-        jobs=( "${jobs[@]:1}" )
+        jobarray=( "${jobarray[@]:1}" )
     fi
     sleep 10
 done
